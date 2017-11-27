b7d7bca96bdbd59296d0ad03ceada38d6aed8d19
kingflag <1362168862@qq.com>
2017-11-27 19:51:40 +0800

    已知前序遍历和中序遍历，构建二叉树

---
+++ src/main/java/tree/rebuldBinaryTree/TreeNode.java
@@ -0,0 +1,15 @@
+package tree.rebuldBinaryTree;
+
+/**
+ * @author wangguoqi
+ * @Date:2017/11/27
+ */
+//首先建立一个二叉树类
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(int x) {
+        val = x;
+    }
+}
