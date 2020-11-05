f6baafb4b152427044481d791da34653601e05d3
kingflag <1362168862@qq.com>
2020-11-05 11:30:45 +0800

    字符串空格替换

---
+++ src/main/java/offer/interviews/ReplaceSpace.java
@@ -0,0 +1,19 @@
+package offer.interviews;
+
+/**
+ * 请实现一个函数，将一个字符串中的每个空格替换成“%20”。例如，当字符串为We Are Happy.则经过替换之后的字符串为We%20Are%20Happy。
+ *
+ * @author kingflag
+ */
+public class ReplaceSpace {
+    public static String replaceSpace(StringBuffer str) {
+        String res = str.toString().replace(" ", "%20");
+        return res;
+    }
+
+    public static void main(String[] args) {
+        StringBuffer test = new StringBuffer("We Are Happy");
+        String res = replaceSpace(test);
+        System.out.println(res);
+    }
+}
