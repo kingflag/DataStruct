2ad0104b27d2f6fbbe60685428dfb876f7efcee9
kingflag <1362168862@qq.com>
2020-11-05 13:26:08 +0800

    链表转数组

---
+++ src/main/java/offer/interviews/ArrayLinked.java
@@ -0,0 +1,59 @@
+package offer.interviews;
+
+import com.alibaba.fastjson.JSONObject;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Stack;
+
+/**
+ * 输入一个链表，按链表从尾到头的顺序返回一个ArrayList。
+ * 示例1：
+ * -- 输入
+ * {67,0,24,58}
+ * -- 返回值
+ * [58,24,0,67]
+ *
+ * @author kingflag
+ */
+public class ArrayLinked {
+
+    private static class ListNode {
+        int val;
+        ListNode next = null;
+
+        ListNode(int val) {
+            this.val = val;
+        }
+    }
+
+    public static ArrayList<Integer> printListFromTailToHead(ListNode listNode) {
+        ArrayList<Integer> list = new ArrayList();
+        Stack<Integer> stack = new Stack<>();
+        ListNode p = listNode;
+        while (p != null) {
+            stack.push(p.val);
+            p = p.next;
+        }
+        while (!stack.empty()) {
+            list.add(stack.pop());
+        }
+        return list;
+    }
+
+    public static void main(String[] args) {
+        ListNode node1 = new ListNode(1);
+        ListNode node2 = new ListNode(2);
+        ListNode node3 = new ListNode(3);
+        ListNode node4 = new ListNode(4);
+        ListNode node5 = new ListNode(5);
+        node1.next = node2;
+        node2.next = node3;
+        node3.next = node4;
+        node4.next = node5;
+        System.out.println(JSONObject.toJSONString(node1));
+        List list = printListFromTailToHead(node1);
+        System.out.println(Arrays.asList(list));
+    }
+}
